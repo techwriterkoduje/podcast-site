81ee27f09613b901688968213f7fded69f2d58cd
Paweł Kowaluk <pensjonatus@gmail.com>
2022-12-29 10:59:16 +0100

    add github actions for github pages

--- themes/websiteTheme.ts
+++ themes/websiteTheme.ts
@@ -1,8 +1,5 @@
 import { createTheme } from '@mui/material';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import '@fontsource/roboto';
 
 export const websiteTheme = createTheme({
   palette: {
