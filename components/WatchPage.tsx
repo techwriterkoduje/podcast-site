a7506a4a6955d175fc5bc03a70342a1d02eba5e4
Paweł Kowaluk <pensjonatus@gmail.com>
2023-02-10 10:23:36 +0100

    add the ITCQF video

--- components/WatchPage.tsx
+++ components/WatchPage.tsx
@@ -4,6 +4,29 @@ import Typography from '@mui/material/Typography';
 
 const videos: FeatureProps[] = [
   {
+    embedId: 'Luya41MjDO4',
+    title: 'Is tech writing in Central and Eastern Europe growing? (soap!)',
+    description: (
+      <>
+        <p>Nasza prezentacja z konferencji soap! w 2022 roku.</p>
+        <p>
+          The world is now more open to remote work and that gives us, technical
+          writing professionals, more opportunities than ever before. If I want
+          to work remotely, which countries should I look into? And what if I
+          want to relocate? Or what if I want to look for writers for my team?
+        </p>
+        <p>
+          ITCQF compiled statistics about technical writers employed in
+          countries across Europe. Come to this talk to learn about which
+          countries have the most writers, which ones have the most job offers,
+          what the salaries are and how the markets have grown or shrunk. You
+          can also learn where the growing trends have been the most rapid.
+        </p>
+      </>
+    ),
+    actionLinks: [{ label: 'soapconf.com', to: 'http://soapconf.com' }],
+  },
+  {
     embedId: 'JkU2n2mpuU0',
     title: 'Doing docs like code (soap!)',
     description: <>Nasza prezentacja z konferencji soap! w 2019 roku.</>,
