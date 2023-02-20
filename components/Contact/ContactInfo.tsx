db021698af90be4273c28e5bb1936b9563f7496c
Paweł Kowaluk <pensjonatus@gmail.com>
2023-02-20 07:52:04 +0100

    change layout of contact info

--- components/Contact/ContactInfo.tsx
+++ components/Contact/ContactInfo.tsx
@@ -11,35 +11,30 @@ const contactEmail = process.env.CONTACT_EMAIL || 'email missing';
 export default function ContactInfo() {
   return (
     <Grid md={12}>
-      <PodcastCard>
-        <Grid container alignItems="center" spacing={4} padding={4}>
-          <Grid md={6}>
-            <Stack spacing={4}>
-              <Typography>
-                Chcesz nam coś przekazać? Masz pytanie lub sugestię? Może masz
-                pomysł na odcinek podcastu albo chcesz być gościem naszej
-                audycji? Po prostu napisz do nas.
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid md={6}>
-            <Stack spacing={4} sx={{ textAlign: 'center' }}>
-              <PodcastButton
-                startIcon={<EmailIcon />}
-                href={`mailto:${contactEmail}`}
-              >
-                Napisz na {contactEmail}
-              </PodcastButton>
-              <CopyToClipboardButton
-                textToCopy={contactEmail}
-                variant="outlined"
-              >
-                skopiuj e-mail do schowka
-              </CopyToClipboardButton>
-            </Stack>
-          </Grid>
+      <Grid container alignItems="center" spacing={4} padding={4}>
+        <Grid md={6}>
+          <Stack spacing={4}>
+            <Typography>
+              Chcesz nam coś przekazać? Masz pytanie lub sugestię? Może masz
+              pomysł na odcinek podcastu albo chcesz być gościem naszej audycji?
+              Po prostu napisz do nas.
+            </Typography>
+          </Stack>
         </Grid>
-      </PodcastCard>
+        <Grid md={6}>
+          <Stack spacing={4} sx={{ textAlign: 'center' }}>
+            <PodcastButton
+              startIcon={<EmailIcon />}
+              href={`mailto:${contactEmail}`}
+            >
+              Napisz na {contactEmail}
+            </PodcastButton>
+            <CopyToClipboardButton textToCopy={contactEmail} variant="outlined">
+              skopiuj e-mail do schowka
+            </CopyToClipboardButton>
+          </Stack>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
--- components/Contact/ContactPage.tsx
+++ components/Contact/ContactPage.tsx
@@ -2,6 +2,7 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 import ContactInfo from './ContactInfo';
+import PodcastCard from '../PodcastCard';
 
 export default function ContactPage() {
   return (
@@ -10,7 +11,9 @@ export default function ContactPage() {
         <Grid md={12} justifyContent="center">
           <Typography variant="h1">Kontakt</Typography>
         </Grid>
-        <ContactInfo />
+        <PodcastCard>
+          <ContactInfo />
+        </PodcastCard>
       </Grid>
     </Container>
   );
