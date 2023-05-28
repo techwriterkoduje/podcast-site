6f5478078946ec0a5e326e6b8292eee10ee89b6f
Paweł Kowaluk <pensjonatus@gmail.com>
2023-05-28 18:33:09 +0200

    less padding for long titles on mobile

--- components/HomePage/LatestEpisodes.tsx
+++ components/HomePage/LatestEpisodes.tsx
@@ -6,20 +6,45 @@ import PodcastCard from '../PodcastCard';
 import AllEpisodesButton from './AllEpisodesButton';
 import Typography from '@mui/material/Typography';
 import SectionStack from '../SectionStack';
+import { useTheme } from '@mui/material/styles';
 
 export type LatestEpisodesProps = { episodeList: HomeEpisodePreviewProps[] };
 
 export default function LatestEpisodes({ episodeList }: LatestEpisodesProps) {
+  const theme = useTheme();
+
   return (
     <PodcastCard>
-      <SectionStack>
+      <SectionStack
+        sx={{
+          [theme.breakpoints.down('md')]: {
+            padding: '1rem 0',
+          },
+        }}
+      >
         <Typography variant="h2" textAlign="center">
           Najnowsze odcinki
         </Typography>
-        <Grid container spacing={4}>
+        <Grid
+          container
+          spacing={4}
+          sx={{
+            [theme.breakpoints.down('md')]: {
+              padding: '0',
+            },
+          }}
+        >
           {episodeList.map(
             ({ title, audioUrl, episodeLink, pubDate, duration }) => (
-              <Grid key={episodeLink} md={12}>
+              <Grid
+                key={episodeLink}
+                md={12}
+                sx={{
+                  [theme.breakpoints.down('md')]: {
+                    padding: '0',
+                  },
+                }}
+              >
                 <HomeEpisodePreview
                   title={title}
                   audioUrl={audioUrl}
