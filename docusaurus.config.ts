1ef9abbdc8b69ec4ebb79242b04b8eaf93e45899
Paweł Kowaluk <pensjonatus@gmail.com>
2025-10-03 22:03:21 +0200

    disable switching color mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
-      respectPrefersColorScheme: true,
+      defaultMode: 'dark',
+      respectPrefersColorScheme: false,
+      disableSwitch: true,
     },
     navbar: {
       title: 'Podcast Tech Writer Koduje',
